07160b1e8205802f73f4cf17ac1041f047be8547
A5TA <cardillojamesbrandon@gmail.com>
2023-09-16 14:32:23 -0400

    fixed some styles

--- client/TrustWave/src/components/Individuals/Individual.tsx
+++ client/TrustWave/src/components/Individuals/Individual.tsx
@@ -12,6 +12,7 @@ import{RiArrowDropDownLine} from 'react-icons/ri';
 import { useState, useContext } from 'react';
 import Person from '../../assets/TomBegging.png'
 import { TransactionContext } from '../../context/TranscationContext';
+import {BiDotsHorizontalRounded} from 'react-icons/bi'
 
 
 interface ExpandMoreProps extends IconButtonProps {
@@ -29,34 +30,74 @@ const ExpandMore = styled((props: ExpandMoreProps) => {
   }),
 }));
 
+const TextShortener: React.FC<{ text: string; maxWords: number }> = ({ text, maxWords }) => {
+  const words = text.split(' ');
+  const [showAll, setShowAll] = useState(false);
+  const displayText = showAll ? text : words.slice(0, maxWords).join(' ');
+  const buttonText = showAll ? 'Show Less' : 'Show More';
+
+  return (
+    <div className="border p-4">
+      <p>{displayText}</p>
+      {words.length > maxWords && (
+        <button
+          className="text-blue-500 hover:underline"
+          onClick={() => setShowAll(!showAll)}
+        >
+          {buttonText}
+        </button>
+      )}
+    </div>
+  );
+};
+
 export default function RecipeReviewCard() {
   const {connectWallet, currentAccount} = useContext(TransactionContext)
   const [expanded, setExpanded] = useState(false);
+  const userDescription = ` We're urgently seeking your help to fund a life-saving surgery for our cherished cat, Tom. 
+  Tom is facing a critical medical condition, and without this surgery, his future hangs in the balance. 
+  Your generous donations will make it possible for Tom to receive the surgery he needs, ensuring a chance at a healthy and happy life. 
+  Please contribute today to make a significant difference in Tom' well-being and our family's peace of mind.`
+
 
   const handleExpandClick = () => {
     setExpanded(!expanded);
   };
 
   return (
-    <Card sx={{ maxWidth: 400 }}>
-      <CardHeader
-        avatar={
-            <Avatar alt="Individual" src={Person} sx={{ width: 100, height: 100 }} variant="square"/>
-        }
-        action={
-          <IconButton aria-label="settings">
-            Hello
-          </IconButton>
-        }
-        title="Tom the Cat"
-        subheader="Springfield, MO"
-      />
+    <Card
+    sx={{
+      maxWidth: '600px', // Adjust the maximum width as needed
+      backgroundColor: 'rgba(0, 0, 255, 0.05)', // Transparent blue background
+      color: 'white', // Set text color to white'
+      border: '1px solid white',
+    }}
+        className='flex flex-col p-5 m-2 cursor-pointer hover:shadow-xl card-bg text-white'
+      >
+        <div className='md:flex md:flex-row md:space-x-10 w-full'>
+          <div className="text-white flex flex-row">
+            <img
+              src={Person}
+              alt="Individual"
+              className="w-36 h-36 rounded-lg"
+            />
+          </div>
+          <div className="flex flex-row space-x-20 items-center">
+            <div className="text-white whitespace-nowrap">
+              <h2 className="text-xl font-bold">Tom the Cat</h2>
+              <p>Springfield, MO</p>
+            </div>
+            <div className="text-white absolute top-0 right-0 p-3">
+              <IconButton aria-label="settings" className="text-white">
+                <BiDotsHorizontalRounded className='text-white' size={25}/> 
+              </IconButton>
+            </div>
+          </div>
+      </div>
+   
       <CardContent>
-        <Typography variant="body2" color="text.secondary">
-        We're urgently seeking your help to fund a life-saving surgery for our cherished cat, Tom. 
-        Tom is facing a critical medical condition, and without this surgery, his future hangs in the balance. 
-        Your generous donations will make it possible for Tom to receive the surgery he needs, ensuring a chance at a healthy and happy life. 
-        Please contribute today to make a significant difference in Tom' well-being and our family's peace of mind.
+        <Typography variant="body2" className='text-white'>
+          <TextShortener text={userDescription} maxWords={35} />
         </Typography>
       </CardContent>
       <CardActions disableSpacing style={{display: 'flex', justifyContent: 'space-between'}}>
@@ -66,13 +107,14 @@ export default function RecipeReviewCard() {
           onClick={handleExpandClick}
           aria-expanded={expanded}
           aria-label="show more"
+          
         >
-          <RiArrowDropDownLine/>
+          <RiArrowDropDownLine className='text-white' size={30} />
         </ExpandMore>
         </div> 
       <div>
         {currentAccount ? 
-      <button aria-label="share" onClick={() => console.log("sent money")}>
+      <button aria-label="share" onClick={() => console.log("sent money")} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">
           Send Donation
       </button>
       :
